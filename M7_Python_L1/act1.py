7b0e231805b70947ee832e751bdb890945adc392
almacode1703 <testmygitt.2023@gmail.com>
2026-02-03 16:44:46 +0530

    Further Updates

---
+++ M7_Python_L1/act1.py
@@ -0,0 +1,11 @@
+print("Welcome to Python programming!")
+
+print(4)
+
+print("Hello There \n")
+
+print("Let's find out total keywords in python...")
+
+import keyword
+print("Total keywords in Python:", len(keyword.kwlist))
+print(keyword.kwlist)
